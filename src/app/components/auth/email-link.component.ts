97daaa9a0de1819458093f8ac5989f553cec2e73
Amani Bisimwa <drcmindfrench@gmail.com>
2023-08-19 05:32:30 +0200

    Folders & Files structure, Routing & navigation

---
+++ src/app/components/auth/email-link.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-email-link',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      email-link works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class EmailLinkComponent {
+
+}
